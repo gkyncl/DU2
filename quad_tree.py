40bc7828a14aab1fb5ac783e76ca8ca90a7115d3
gkyncl <56367707+gkyncl@users.noreply.github.com>
2019-12-17 20:46:37 +0100

    verze #1

--- quad_tree.py
+++ quad_tree.py
@@ -1,8 +1,7 @@
 import turtle, statistics
 
 def split_lines(json_file):
-    # vypocita linie rezu
-    #features = json_file["features"][0:30]
+    # vypocet linii rezu
     body = []  # list souradnic
     for pnts in json_file:
         souradnice = pnts["geometry"]["coordinates"]
@@ -11,19 +10,16 @@ def split_lines(json_file):
 
 
     body.sort(key=lambda p: p[0])  # serazeni podle x
-    Xmax = body[-1][0]
-    Xmin = body[0][0]
+    xmax = body[-1][0]
+    xmin = body[0][0]
 
     body.sort(key=lambda p: p[1])  # serazeni podle y
-    Ymax = body[-1][1]
-    Ymin = body[0][1]
+    ymax = body[-1][1]
+    ymin = body[0][1]
 
-
-    #drawing(body, Xmax, Xmin, Ymax, Ymin)
-
-    X_mid = (Xmax + Xmin) / 2
-    Y_mid = (Ymax + Ymin) / 2
-    return(X_mid, Y_mid, Xmax, Xmin, Ymax, Ymin, body)
+    x_mid = (xmax + xmin) / 2
+    y_mid = (ymax + ymin) / 2
+    return(x_mid, y_mid, xmax, xmin, ymax, ymin, body)
 
 
 
@@ -33,53 +29,38 @@ a = [1,2] # seznam pro tvorbu id
 
 
 def quad_tree(json_list, xmid, ymid, len_x, len_y, kvadrant = 0):
-    print("delka:", len(json_list))
     # len -- delka odpovidajici pulce strany boundig boxu
     # mid -- delici hodnoty
+    # kvadrant -- defaultne roven 0, meni se az po prvnim rozdeleni
 
-    if len(json_list) < 5:
+    if len(json_list) < 50:
         a.sort(reverse=True)
-        Id = a[-1]# vytazeni id z id_seznamu
+        id = a[-1]# vytazeni id z id_seznamu
+        # zapis id
         for i in json_list:
-            i["properties"]["cluster_id"] = Id
+            i["properties"]["cluster_id"] = id
             final_list.append(i)
         a.pop() # vymazani last prvku
-        a.append(Id+2) # pridani prvku
+        a.append(id+2) # pridani prvku
 
-
-        return (json_list) # navrat
+        return (json_list)
 
     # vypocet linii rezu na zaklade kvadrantu
-    if kvadrant ==0:
-        X_mid, Y_mid = xmid, ymid
-        #Xmax, Xmin, Ymax, Ymin = split_lines(json_list)[2:6] # kraje puvodniho bounding boxu
-        b1_x, b1_y = (xmid - len_x/2, ymid + len_y/2)
-        b2_x, b2_y = (xmid + len_x/2, ymid + len_y/2)
-        b3_x, b3_y = (xmid - len_x/2, ymid - len_y/2)
-        b4_x, b4_y = (xmid + len_x/2, ymid - len_y/2)
-
-
-        print("0")
-    elif kvadrant ==1:
+    if kvadrant ==1:
         xmid = xmid - len_x
         ymid = ymid + len_y
 
-        print("1")
     elif kvadrant ==2:
         xmid = xmid + len_x
         ymid = ymid + len_y
 
-        print("2")
     elif kvadrant ==3:
         xmid = xmid - len_x
         ymid = ymid - len_y
 
-        print("3")
     elif kvadrant ==4:
         xmid = xmid + len_x
         ymid = ymid - len_y
-        print("4")
-
 
     # seznamy pro 4 kvadranty bodu (D = down, U = up, R = right, L = left)
     UL = []
@@ -87,7 +68,7 @@ def quad_tree(json_list, xmid, ymid, len_x, len_y, kvadrant = 0):
     DL = []
     DR = []
 
-    # prochazim json_file a delim body do 4 kvadrantu - vzniknou 4 dilci json soubory
+    # prochazim json_file a delim body do 4 kvadrantu -- vzniknou 4 dilci json soubory
     for pts in json_list:
         souradnice = pts["geometry"]["coordinates"]
         x, y = souradnice
@@ -107,65 +88,6 @@ def quad_tree(json_list, xmid, ymid, len_x, len_y, kvadrant = 0):
     quad_tree(DL,xmid, ymid,len_x/2, len_y/2, kvadrant=3)
     quad_tree(DR,xmid, ymid,len_x/2, len_y/2,kvadrant=4)
 
-
-
     return (final_list)
 
 
-def cut_lines_draw(body):
-    first = []
-    second = []
-    third = []
-    fourth = []
-    for values in body:
-        # rozdeleni na 4 seznamy
-        first.append(values[0])
-        second.append(values[1])
-        third.append(values[2])
-        fourth.append(values[3])
-
-    # preskalovani souradnic pomoci z-skoru
-    mean_first = statistics.mean(first)
-    sd_first = statistics.stdev(first)
-
-    mean_second = statistics.mean(second)
-    sd_second = statistics.stdev(second)
-
-    mean_third = statistics.mean(third)
-    sd_third = statistics.stdev(third)
-
-    mean_fourth = statistics.mean(fourth)
-    sd_fourth = statistics.stdev(fourth)
-
-    for vals in body:
-        x1 = 200*((vals[0]-mean_first)/sd_first)
-        y1 = 200*((vals[2]-mean_third)/sd_third)
-
-        x2 = 200*((vals[0]-mean_first)/sd_first)
-        y2 = 200*((vals[3]-mean_fourth)/sd_fourth)
-
-        x3 = 200*((vals[2]-mean_third)/sd_third)
-        y3 = 200*((vals[2]-mean_third)/sd_third)
-
-        x4 = 200*((vals[2]-mean_third)/sd_third)
-        y4 = 200*((vals[3]-mean_fourth)/sd_fourth)
-
-        turtle.speed(50)
-        turtle.penup()
-        turtle.goto(x1, y1)
-        turtle.pendown()
-        turtle.dot(5, "blue")
-        turtle.penup()
-        turtle.goto(x2, y2)
-        turtle.pendown()
-        turtle.dot(5, "blue")
-        turtle.penup()
-        turtle.goto(x3, y3)
-        turtle.pendown()
-        turtle.dot(5, "blue")
-        turtle.penup()
-        turtle.goto(x4, y4)
-        turtle.pendown()
-        turtle.dot(5, "blue")
-
-        turtle.bye()
--- split.py
+++ split.py
@@ -1,22 +1,19 @@
-import math, json, quad_tree, kresleni_zelva
+import math, json, quad_tree
 
 # nacteni geoJSON souboru
-
-with open("points_out2.json", "r", encoding="utf-8") as f:
+with open("input.json", "r", encoding="utf-8") as f:
     data = json.load(f) # nyni mam data ze souboru nactena
-
 
 features =data["features"]
 
 
-# vypocitej strany obdelnika:
+# vypocitej strany obdelnika -- bounding boxu
 xmid, ymid, xmax, xmin, ymax, ymin = quad_tree.split_lines(features)[0:6]
 len_x = abs(xmax - xmin)/2
 len_y = abs(ymax - ymin)/2
 
-
+# volani funkce na quad_tree deleni
 a_list = quad_tree.quad_tree(features, xmid, ymid, len_x, len_y)
-
 
 
 # vytvoreni json vystupniho souboru
@@ -24,6 +21,6 @@ gj_structure = {"type": "FeatureCollection"}
 gj_structure["features"] = a_list
 
 # zapis souboru
-with open("points_out3.json", "w", encoding="utf-8") as f:
-    json.dump(gj_structure, f, indent=2, ensure_ascii=False) # index udela hezke odsazeni :-)
+with open("output.json", "w", encoding="utf-8") as f:
+    json.dump(gj_structure, f, indent=2, ensure_ascii=False)
 
